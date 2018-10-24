a72bcd566aa5b97be5f28126b71527b5bd6bfcfb
Nadrojiskool <jjbhaveagoodtime@yahoo.com>
2018-10-24 10:14:07 -0700

    Testing

--- Test Server/Program.cs
+++ Test Server/Program.cs
@@ -13,14 +13,24 @@ namespace Test_Server
         static void Main(string[] args)
         {
             UdpClient udpServer = new UdpClient(57000);
+            List<User> UserList = new List<User>();
+            int connectedUsers = 0;
 
             while (true)
             {
                 var remoteEP = new IPEndPoint(IPAddress.Any, 57000);
-                var data = udpServer.Receive(ref remoteEP); // listen on port 11000
+                var data = udpServer.Receive(ref remoteEP); // listen on port 57000
+                List<byte[]> packet = new List<byte[]>();
                 Console.Write($"receive data from {remoteEP}\n");
-                Console.Write($"{data.Count()}\n");
-                udpServer.Send(new byte[] { 1 }, 1, remoteEP); // reply back
+                Console.Write($"Adding Username & Endpoint to Cache\n");
+                UserList.Add(new User(Encoding.Default.GetString(data), remoteEP));
+                if (UserList.Count() > connectedUsers) { Console.WriteLine($"User Cached!"); connectedUsers++; udpServer.Send(new byte[] { 1 }, 1, remoteEP); }
+                else { Console.WriteLine($"MERP!"); }
+                for (int i = 0; i < 20; i++)
+                {
+                    packet.Add(udpServer.Receive(ref remoteEP));
+                }
+                Console.Write($"{packet.Count()}\n");
             }
         }
     }
---
+++ Test Server/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Server
+{
+    public class User
+    {
+        public string Username;
+        public IPEndPoint Endpoint;
+
+        public User(string username, IPEndPoint endpoint)
+        {
+            Username = username;
+            Endpoint = endpoint;
+        }
+    }
+}
