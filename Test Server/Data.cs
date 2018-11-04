9e20035f980c51d25ea29bd30b33fbbf5233d922
Jordan Benner <jjbhaveagoodtime@yahoo.com>
2018-11-04 13:22:18 -0800

    Temporary Branch

---
+++ Test Server/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Server
+{
+    public class Data
+    {
+        public byte[] Byte = new byte[50000];
+        public IPEndPoint Endpoint;
+
+        public Data(byte[] b, IPEndPoint ep)
+        {
+            Byte = b;
+            Endpoint = ep;
+        }
+    }
+}
--- Test Server/Program.cs
+++ Test Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.IO;
 using System.Net;
@@ -8,111 +9,211 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+
+/*  Server Commands
+ * 0: Establish Connection (Not In Use)
+ * 1: Ready to Receive
+ * 2: Receipt Confirmation
+ * 3: 
+ * 4: 
+ * 5:
+ * 6:
+ * 7:
+ * 8:
+ * 9:
+ * 10: Load Map
+ * 11
+ * 12
+ */
+
+
 namespace Test_Server
 {
     public class Program
     {
+        public static bool AcceptConnections = true;
         public static bool messageReceived = false;
+        public static bool messageConfirmation = false;
+        public static int connectedUsers = 0;
+        public static List<User> UserList = new List<User>();
+        public static UdpClient udpServer = new UdpClient(57000);
 
         static void Main(string[] args)
         {
-            UdpClient udpServer = new UdpClient(57000);
-            List<User> UserList = new List<User>();
-            int connectedUsers = 0;
+            Listener(udpServer);
 
-            while (true)
+            while (AcceptConnections)
+            {
+                Task.Delay(1000);
+            }
+        }
+
+        public static async Task Listener(UdpClient client)
+        {
+            Stopwatch elapsed = new Stopwatch();
+            elapsed.Start();
+            int delay = 100;
+            while (AcceptConnections == true)
             {
                 IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 57000);
-                var data = udpServer.Receive(ref remoteEP); // listen on port 57000
-                List<byte[]> packet = new List<byte[]>();
-                Console.Write($"receive data from {remoteEP}\n");
-                Console.Write($"Adding Username & Endpoint to Cache\n");
-                UserList.Add(new User(Encoding.Default.GetString(data), remoteEP));
-                
-                if (UserList.Count() > connectedUsers)
+                Console.WriteLine("Listening for Information..");
+                Data data = new Data(await Task.Run(() => client.Receive(ref remoteEP)), remoteEP);
+                Console.WriteLine($"Received Information.. {data.Byte[0]} // {remoteEP}");
+                if (data.Byte[0] == 2)
                 {
-                    Console.WriteLine($"User {Encoding.Default.GetString(data)} Cached!");
-                    connectedUsers++;
+                    messageConfirmation = true;
                 }
-                else { Console.WriteLine($"MERP!"); }
-
-                InitiateCallback(udpServer, remoteEP);
-                while (!messageReceived)
+                else
                 {
-                    Thread.Sleep(50);
-                }
-                DateTime dateTime = DateTime.UtcNow;
-
-                byte[] informationToSend = new byte[50000];
-                string[] informationToWriteBiome = new string[1000000];
-                informationToWriteBiome = File.ReadAllLines("C:/Users/Hal/Desktop/test1biome.txt");
-
-                for (int i = 0; i < 20; i++)
-                {
-                    for (int ii = 0; ii < 50000; ii++)
+                    if (data.Byte[0] == delay + 100)
                     {
-                        informationToSend[ii] = byte.Parse(informationToWriteBiome[(i * 50000) + ii]);
+                        delay = data.Byte[0];
+                        Console.WriteLine($"New Delay: {delay}");
+                        data.Byte[0] += 100;
+                        Speaker(data.Byte, client, remoteEP);
                     }
-
-                    udpServer.Send(informationToSend, 50000, remoteEP);
-                    Console.WriteLine($"Packet Sent..");
-                    Console.WriteLine("Listening for Messages..");
-                    InitiateCallback(udpServer, remoteEP);
-
-                    while (messageReceived != true)
-                    {
-                        if (DateTime.UtcNow.Millisecond > dateTime.Millisecond + 1000)
-                        {
-                            udpServer.Send(informationToSend, 50000, remoteEP);
-                            Console.WriteLine($"Packet Sent..");
-                        }
-                        Thread.Sleep(50);
-                    }
-
-                    messageReceived = false;
-                }
-
-                dateTime = DateTime.UtcNow;
-                string[] informationToWriteMod = new string[1000000];
-                informationToWriteMod = File.ReadAllLines("C:/Users/Hal/Desktop/test1mod.txt");
-
-                for (int i = 0; i < 20; i++)
-                {
-                    for (int ii = 0; ii < 50000; ii++)
-                    {
-                        informationToSend[ii] = byte.Parse(informationToWriteMod[(i * 50000) + ii]);
-                    }
-
-                    udpServer.Send(informationToSend, 50000, remoteEP);
-                    Console.WriteLine($"Packet Sent..");
-                    Console.WriteLine("Listening for Messages..");
-                    InitiateCallback(udpServer, remoteEP);
-
-                    while (messageReceived != true)
-                    {
-                        if (DateTime.UtcNow.Millisecond > dateTime.Millisecond + 1000)
-                        {
-                            udpServer.Send(informationToSend, 50000, remoteEP);
-                            Console.WriteLine($"Packet Sent..");
-                        }
-                        Thread.Sleep(50);
-                    }
-
-                    messageReceived = false;
                 }
             }
         }
 
-        public static async Task InitiateCallback(UdpClient client, IPEndPoint ep)
+        public static async Task Speaker(byte[] packet, UdpClient client, IPEndPoint endpoint)
         {
-            await Task.Run(() => ReceiveCallback(client, ep));
+            while (!messageConfirmation)
+            {
+                Console.WriteLine($"Requesting Delay Increase to: {packet[0]}");
+                client.Send(packet, 1, endpoint);
+                await Task.Delay(packet[0]);
+            }
+            messageConfirmation = false;
         }
 
-        public static void ReceiveCallback(UdpClient client, IPEndPoint ep)
+
+
+
+        /*public static bool AcceptConnections = true;
+        public static bool messageReceived = false;
+        public static bool messageConfirmation = false;
+        public static int connectedUsers = 0;
+        public static List<User> UserList = new List<User>();
+        public static UdpClient udpServer = new UdpClient(57000);
+
+        static void Main(string[] args)
         {
-            client.Receive(ref ep);
-            Console.WriteLine("Received Response!");
-            messageReceived = true;
+            NewConnectionListener(udpServer);
+            
+            while (AcceptConnections)
+            {
+                Task.Delay(1000);
+            }
         }
+
+        public static async Task NewConnectionListener(UdpClient client)
+        {
+            while (AcceptConnections == true)
+            {
+                Data data = await DataListener(client);
+
+                if (UserList.Count() == 0)
+                {
+                    InitializeClient(client, data.Endpoint, data);
+                    //UserList[0].Endpoint = data.Endpoint;
+                }
+
+                if (data.Byte[0] == 10 && data.Byte[1] == 1)
+                {
+                    Console.WriteLine($"Start Send {data.Endpoint}");
+                    DataSender(client, data.Endpoint);
+                }
+                else if (data.Byte[0] == 2)
+                {
+                    messageConfirmation = true;
+                }
+            }
+        }
+
+        public static async Task InitializeClient(UdpClient client, IPEndPoint endpoint, Data data)
+        {
+            UserList.Add(new User(Encoding.Default.GetString(data.Byte), data.Endpoint));
+            Console.Write($"Received data from {data.Endpoint}!\n");
+            Console.Write($"Adding Username & Endpoint to Cache!\n");
+            if (UserList.Count() > connectedUsers)
+            {
+                Console.WriteLine($"User {Encoding.Default.GetString(data.Byte)} Cached!");
+                connectedUsers++;
+            }
+            else { Console.WriteLine($"MERP!"); }
+        }
+
+        public static async Task<Data> DataListener(UdpClient client)
+        {
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 57000);
+            Console.WriteLine("Listening for Information..");
+            Data data = new Data(await Task.Run(() => client.Receive(ref remoteEP)), remoteEP);
+            Console.WriteLine("Received Information..");
+            Console.WriteLine($"{data.Byte[0].ToString()} // {remoteEP}");
+            return data;
+        }
+
+        public static async Task DataSender(UdpClient client, IPEndPoint endpoint)
+        {
+            byte[] informationToSend = new byte[50000];
+            string[] informationToWriteBiome = new string[1000000];
+            informationToWriteBiome = File.ReadAllLines("C:/Users/Hal/Desktop/test1biome.txt");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int ii = 0; ii < 50000; ii++)
+                {
+                    informationToSend[ii] = byte.Parse(informationToWriteBiome[(i * 50000) + ii]);
+                }
+
+                client.Send(informationToSend, 50000, endpoint);
+                Console.WriteLine($"Packet Sent..");
+                Console.WriteLine("Listening for Messages..");
+
+                while (!messageConfirmation)
+                {
+                    if (stopwatch.ElapsedMilliseconds > 1000)
+                    {
+                        client.Send(informationToSend, 50000, endpoint);
+                        Console.WriteLine($"Packet Sent..");
+                        stopwatch.Restart();
+                    }
+                    Thread.Sleep(50);
+                }
+
+                messageConfirmation = false;
+            }
+            
+            string[] informationToWriteMod = new string[1000000];
+            informationToWriteMod = File.ReadAllLines("C:/Users/Hal/Desktop/test1mod.txt");
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int ii = 0; ii < 50000; ii++)
+                {
+                    informationToSend[ii] = byte.Parse(informationToWriteMod[(i * 50000) + ii]);
+                }
+
+                client.Send(informationToSend, 50000, endpoint);
+                Console.WriteLine($"Packet Sent..");
+                Console.WriteLine("Listening for Messages..");
+
+                while (!messageConfirmation)
+                {
+                    if (stopwatch.ElapsedMilliseconds > 1000)
+                    {
+                        client.Send(informationToSend, 50000, endpoint);
+                        Console.WriteLine($"Packet Sent..");
+                    }
+                    Thread.Sleep(50);
+                }
+
+                messageConfirmation = false;
+            }
+        }*/
     }
 }
